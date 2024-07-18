b9752db23585bb25a6e05c438d023e147b937d22
Mateus Henrique <mateus.henriqueab1616@gmail.com>
2024-07-18 11:06:05 -0300

    Create teste4.java

---
+++ teste4.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class User {
+    private String name;
+    private List<Account> conta;
+
+    // getters and setters
+}
+
+public class Account {
+    private int idConta;
+
+    // getters and setters
+}
+
+public class UserAccount {
+    private String name;
+    private int idConta;
+
+    // getters and setters
+}
